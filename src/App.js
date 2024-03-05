5a1849f3a5a860c850f5ac3c625df5b78ea83d84
suraj189 <suraj84467@gmail.com>
2024-03-05 12:59:12 +0530

    Created navigation bar for laptop and mobile device

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
 
 function App() {
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item"><a href="#">Home</a></li>
+        <li className="nav-item"><a href="#">About</a></li>
+        <li className="nav-item"><a href="#">Services</a></li>
+        <li className="nav-item"><a href="#">Contact</a></li>
+      </ul>
+      <div>
+        <button className="btn">Contact</button>
+      </div>
+    </nav>
     </div>
   );
 }
